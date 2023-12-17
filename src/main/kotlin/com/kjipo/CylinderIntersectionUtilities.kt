981785d8f545b5ca8c46ac2af21a2989cccb438d
Tore Brede <tore.brede@gmail.com>
2023-12-17 22:18:46 +0100

    Working on minimizer function

--- src/main/kotlin/com/kjipo/CylinderIntersectionUtilities.kt
+++ src/main/kotlin/com/kjipo/CylinderIntersectionUtilities.kt
@@ -50,12 +50,12 @@ class CylinderIntersectionUtilities {
      * Compute one-sided limits at t = 1 for line P + t(Q_0 - P) where Q_0 cross W_0 = 0 with Q_0 last component 1
      */
     fun limitsDgdtOneQ0(
-            r0: Double, r1: Double, pVector: Vector3, w0Vector: Vector3,
-            lVector: Vector3, w1Vector: Vector3, q0Vector: Vector3, q1Vector: Vector3
+        r0: Double, r1: Double, pVector: Vector3, w0Vector: Vector3,
+        lVector: Vector3, w1Vector: Vector3, q0Vector: Vector3, q1Vector: Vector3
     ): Pair<Double, Double> {
         val r0Term = r0 * lVector.crossProduct(w0Vector).length()
         val r1Term = r1 * q0Vector.crossProduct(w1Vector).dot(lVector.crossProduct(w1Vector))
-                .div(q1Vector.crossProduct(w0Vector).length())
+            .div(q1Vector.crossProduct(w0Vector).length())
         val h0Term = (h0 / 2) * lVector.dot(w0Vector).absoluteValue
         val h1Term = (h1 / 2) * lVector.dot(w1Vector).absoluteValue
 
@@ -72,8 +72,8 @@ class CylinderIntersectionUtilities {
      */
     fun limitsDgdtOneQ1(q1Vector: Vector3): Pair<Double, Double> {
         val r0Term =
-                r0 * q1Vector.crossProduct(w0Vector).dot(lVector.crossProduct(w0Vector)) / q1Vector.crossProduct(w0Vector)
-                        .length()
+            r0 * q1Vector.crossProduct(w0Vector).dot(lVector.crossProduct(w0Vector)) / q1Vector.crossProduct(w0Vector)
+                .length()
         val r1Term = r1 * lVector.crossProduct(w1Vector).length()
         val h0Term = (h0 / 2) * lVector.dot(w0Vector).absoluteValue
         val h1Term = (h1 / 2) * lVector.dot(w1Vector).absoluteValue
@@ -100,7 +100,7 @@ class CylinderIntersectionUtilities {
     }
 
 
-    fun computeMinimumSingularZero(dgdt0n: Double, dgdt0p: Double): Double? {
+    fun computeMinimumSingularZero(dgdt0n: Double, dgdt0p: Double): Double {
         var t0 = -1.0
 
         val tMin = if (dgdt0n > 0) {
@@ -136,8 +136,8 @@ class CylinderIntersectionUtilities {
     }
 
     fun computeMinimumSingularZeroOne(
-            dgdt0n: Double, dgdt0p: Double,
-            dgdt1n: Double, dgdt1p: Double
+        dgdt0n: Double, dgdt0p: Double,
+        dgdt1n: Double, dgdt1p: Double
     ): Double {
         val tMin = if (dgdt0n > 0) {
             // The root of g'(t) occurs on (-infinity, 0). Locate t_0 for which g'(t_0) < 0
@@ -193,12 +193,11 @@ class CylinderIntersectionUtilities {
     }
 
 
-
     /**
      * Evaluation of g(t) for all t
      */
     fun gFunction(
-            t: Double
+        t: Double
 //        , pVector: Vector3, lVector: Vector3,
 //        r0: Double, r1: Double,
 //        w0: Vector3, w1: Vector3,
@@ -220,14 +219,18 @@ class CylinderIntersectionUtilities {
      * potential singularity t = 1
      */
     fun dgdt(
-            t: Double
+        t: Double
 //        , pVector: Vector3, lVector: Vector3,
 //        r0: Double, r1: Double, w0: Vector3, w1: Vector3,
 //        h0: Double, h1: Double
     ): Double {
         val dVector = pVector + lVector * t
-        val r0Term = r0 * dVector.crossProduct(w0Vector).dot(lVector.crossProduct(w0Vector)) / dVector.crossProduct(w0Vector).length()
-        val r1Term = r1 * dVector.crossProduct(w1Vector).dot(lVector.crossProduct(w1Vector)) / dVector.crossProduct(w1Vector).length()
+        val r0Term =
+            r0 * dVector.crossProduct(w0Vector).dot(lVector.crossProduct(w0Vector)) / dVector.crossProduct(w0Vector)
+                .length()
+        val r1Term =
+            r1 * dVector.crossProduct(w1Vector).dot(lVector.crossProduct(w1Vector)) / dVector.crossProduct(w1Vector)
+                .length()
         val sgn = if (t > 0) 1 else -1
         val h0Term = (h0 / 2) * abs(lVector.dot(w0Vector) * sgn)
         val h1Term = (h1 / 2) * abs(lVector.dot(w1Vector) * sgn)
@@ -247,12 +250,18 @@ class CylinderIntersectionUtilities {
 
         if (lengthW0CrossW1 > 0) {
             // Test for separation by W_0
-            if (cylinder2.r0 * lengthW0CrossW1 + cylinder.h0 / 2 + (cylinder2.h0 / 2) * cylinder.w0Vector.dot(cylinder2.w0Vector).absoluteValue - cylinder.w0Vector.dot(delta).absoluteValue < 0) {
+            if (cylinder2.r0 * lengthW0CrossW1 + cylinder.h0 / 2 + (cylinder2.h0 / 2) * cylinder.w0Vector.dot(cylinder2.w0Vector).absoluteValue - cylinder.w0Vector.dot(
+                    delta
+                ).absoluteValue < 0
+            ) {
                 return cylinder.w0Vector
             }
 
             // Test for separation by W_1
-            if (cylinder.r0 * lengthW0CrossW1 + (cylinder.h0 / 2) * cylinder.w0Vector.dot(cylinder2.w0Vector).absoluteValue + (cylinder2.h0 / 2) * delta.dot(cylinder2.w0Vector).absoluteValue < 0) {
+            if (cylinder.r0 * lengthW0CrossW1 + (cylinder.h0 / 2) * cylinder.w0Vector.dot(cylinder2.w0Vector).absoluteValue + (cylinder2.h0 / 2) * delta.dot(
+                    cylinder2.w0Vector
+                ).absoluteValue < 0
+            ) {
                 return cylinder2.w0Vector
             }
 
@@ -263,32 +272,32 @@ class CylinderIntersectionUtilities {
 
             // Test for separation by delta
             if (cylinder.r0 * delta.crossProduct(cylinder.w0Vector).length()
-                    + cylinder2.r0 * delta.crossProduct(cylinder2.w0Vector).length()
-                    + (h0 / 2) * delta.dot(cylinder.w0Vector).absoluteValue
-                    + (h1 / 2) * delta.dot(cylinder.w0Vector).absoluteValue
-                    - delta.dot(delta) < 0) {
+                + cylinder2.r0 * delta.crossProduct(cylinder2.w0Vector).length()
+                + (h0 / 2) * delta.dot(cylinder.w0Vector).absoluteValue
+                + (h1 / 2) * delta.dot(cylinder.w0Vector).absoluteValue
+                - delta.dot(delta) < 0
+            ) {
                 return delta.normalize()
             }
 
             return separatedByOtherDirections(
-                    delta,
-                    cylinder.w0Vector,
-                    cylinder.r0,
-                    cylinder.h0,
-                    cylinder2.w0Vector,
-                    cylinder2.r0,
-                    cylinder2.h0
+                delta,
+                cylinder.w0Vector,
+                cylinder.r0,
+                cylinder.h0,
+                cylinder2.w0Vector,
+                cylinder2.r0,
+                cylinder2.h0
             )
 
-        }
-        else {
+        } else {
             // Test for separation by height
-            if((cylinder.h0 + cylinder2.h0)/2 - delta.dot(w0Vector).absoluteValue < 0) {
-               return w0Vector
+            if ((cylinder.h0 + cylinder2.h0) / 2 - delta.dot(w0Vector).absoluteValue < 0) {
+                return w0Vector
             }
 
             // Test for separation radially
-            if((cylinder.r0 + cylinder2.r0) - delta.crossProduct(w0Vector).length() < 0) {
+            if ((cylinder.r0 + cylinder2.r0) - delta.crossProduct(w0Vector).length() < 0) {
                 return (delta - w0Vector * delta.dot(w0Vector)).normalize()
             }
 
@@ -320,8 +329,16 @@ class CylinderIntersectionUtilities {
 
         // Convert to the coordinates system where N = Delta / |Delta| is the north
         // pole of the hemisphere to be searched
-        this.w0Vector = Vector3(uVector.dot(cylinder1AxisVector), vVector.dot(cylinder1AxisVector), nVector.dot(cylinder1AxisVector))
-        this.w1Vector = Vector3(uVector.dot(cylinder2AxisVector), vVector.dot(cylinder2AxisVector), nVector.dot(cylinder2AxisVector))
+        this.w0Vector = Vector3(
+            uVector.dot(cylinder1AxisVector),
+            vVector.dot(cylinder1AxisVector),
+            nVector.dot(cylinder1AxisVector)
+        )
+        this.w1Vector = Vector3(
+            uVector.dot(cylinder2AxisVector),
+            vVector.dot(cylinder2AxisVector),
+            nVector.dot(cylinder2AxisVector)
+        )
         this.w0CrossW1 = cylinder1AxisVector.crossProduct(cylinder2AxisVector)
 
         if (cylinder1AxisVector.z > 0) {
@@ -393,11 +410,11 @@ class CylinderIntersectionUtilities {
 
         val result = findMinimum(angle0, angle1, angle2)
 
-        if(result == null) {
+        if (result == null) {
             return null
         }
 
-        if(result.gValue < 0) {
+        if (result.gValue < 0) {
 
             // TODO
 
@@ -426,10 +443,10 @@ class CylinderIntersectionUtilities {
 
 
     fun computeLineMinimum(
-            lVector: Vector3,
-            q0Vector: Vector3, q1Vector: Vector3,
-            pVector: Vector3
-    ): Double? {
+        lVector: Vector3,
+        q0Vector: Vector3, q1Vector: Vector3,
+        pVector: Vector3
+    ): Double {
         var lPerpVector = Vector3(lVector.x, lVector.y, 0.0)
 
         return if (lPerpVector.dot(q0Vector - pVector) != 0.0) {
@@ -449,8 +466,8 @@ class CylinderIntersectionUtilities {
                 val (dgdt0n, dgdt0p) = limitsDgdtZero()
                 val (dgdt1n, dgdt1p) = limitsDgdtOneQ1(q1Vector)
                 computeMinimumSingularZeroOne(
-                        dgdt0n, dgdt0p,
-                        dgdt1n, dgdt1p
+                    dgdt0n, dgdt0p,
+                    dgdt1n, dgdt1p
                 )
             }
 
@@ -469,6 +486,9 @@ class CylinderIntersectionUtilities {
 
         val imax = 10
         val maxTries = 1000
+        val maxBisections = 20
+        val epsilon = 1e-08
+        val tolerance = 1e-04
 
 
         fun searchForSeparatingAxis(cylinder1: Cylinder, cylinder2: Cylinder): Vector3? {
@@ -479,7 +499,7 @@ class CylinderIntersectionUtilities {
         }
 
         /**
-         * TODO Will this bisect method always work?
+         * TODO As it is now this method does not distinguish between minimum or maximum points, so it cannot be guaranteed to find a minimum
          */
         fun bisect(
             valueFunction: (Double) -> Double,
@@ -489,7 +509,7 @@ class CylinderIntersectionUtilities {
             bracketEnd: Double
 //               t1: Double,
 //    dgdt0n: Double, tMin: Double, dgdtTmin: Double
-        ): Double? {
+        ): Double {
 //        var a = t0
 //        var b = t0 / 2
 //        var c = 0.0
@@ -516,13 +536,176 @@ class CylinderIntersectionUtilities {
                 ++tryCounter
             }
 
-            if(tryCounter == maxTries) {
-                return null
-            }
+//            if (tryCounter == maxTries) {
+            // TODO Write warning or throw an exception?
+//            }
 
             return c
         }
-    }
 
+        fun bisect2(
+            valueFunction: (Double) -> Double,
+            bracketStart: Double,
+            pointInBracket: Double,
+            bracketEnd: Double
+        ): Pair<Double, Double> {
+            val a = Pair(bracketStart, valueFunction(bracketStart))
+            val b = Pair(pointInBracket, valueFunction(pointInBracket))
+            val c = Pair(bracketEnd, valueFunction(bracketEnd))
+            val currentMin = listOf(a, b, c).maxBy { it.second }
+
+            return if ((b.second < a.second && c.second >= b.second) ||
+                (c.second > b.second && a.second >= b.second)
+            ) {
+                getBracketedMinimum(valueFunction, a, b, c, currentMin)
+
+            } else {
+                listOf(
+                    subdivide(valueFunction, a, b, currentMin, maxBisections),
+                    subdivide(valueFunction, b, c, currentMin, maxBisections)
+                ).minBy { it.second }
+            }
+        }
+
+        private fun subdivide(
+            valueFunction: (Double) -> Double,
+            a: Pair<Double, Double>,
+            b: Pair<Double, Double>,
+            currentMin: Pair<Double, Double>,
+            maxBisections: Int
+        ): Pair<Double, Double> {
+            if (maxBisections - 1 == 0) {
+                return currentMin
+            }
+
+            val midPoint = 0.5 * (a.first + b.first)
+            val midPointValue = 0.5 * (a.second + b.second)
+
+
+            val updatedCurrentMin = if (midPointValue < currentMin.second) {
+                Pair(midPoint, midPointValue)
+            } else {
+                currentMin
+            }
+
+            return if ((midPointValue < a.second && b.second >= midPointValue) ||
+                (b.second > midPointValue && a.second >= midPointValue)
+            ) {
+                getBracketedMinimum(valueFunction, a, Pair(midPoint, midPointValue), b, updatedCurrentMin)
+            } else {
+                val remainingBisections = maxBisections - 1
+                return listOf(
+                    subdivide(valueFunction, a, Pair(midPoint, midPointValue), updatedCurrentMin, remainingBisections),
+                    subdivide(valueFunction, a, Pair(midPoint, midPointValue), updatedCurrentMin, remainingBisections)
+                ).minBy { it.second }
+            }
+        }
+
+        fun getBracketedMinimum(
+            valueFunction: (Double) -> Double,
+            a: Pair<Double, Double>,
+            b: Pair<Double, Double>,
+            c: Pair<Double, Double>,
+            currentMin: Pair<Double, Double>
+        ): Pair<Double, Double> {
+            var currentMinInput = currentMin.first
+            var currentMinValue = currentMin.second
+            var aPair = a
+            var bPair = b
+            var cPair = c
+
+            for (i in 0..maxBisections) {
+                if (cPair.second < currentMinValue) {
+                    currentMinInput = cPair.first
+                    currentMinValue = cPair.second
+                }
+
+                val diff = cPair.first - aPair.first
+                val bound = 2 * tolerance * abs(bPair.first) + epsilon
+                if (diff <= bound) {
+                    return Pair(currentMinInput, currentMinValue)
+                }
+
+                val diffInputAB = aPair.first - bPair.first
+                val diffInputCB = cPair.first - bPair.first
+                val diffValueAB = aPair.second - bPair.second
+                val diffValueCB = cPair.second - bPair.second
+                val temp = diffInputAB * diffValueCB
+                val temp2 = diffInputCB * diffValueAB
+                val denom = temp2 - temp
+
+                if (abs(denom) <= epsilon) {
+                    return Pair(currentMinInput, currentMinValue)
+                }
+
+                val tempNewInput = bPair.first + 0.5 * (diffInputCB * temp2 - diffInputAB * temp) / denom
+                val newInput = max(aPair.first, min(tempNewInput, cPair.first))
+                val newValue = valueFunction(newInput)
+
+                if (newValue < currentMinValue) {
+                    currentMinInput = newInput
+                    currentMinValue = newValue
+                }
+
+                if (newInput < bPair.first) {
+                    if (newValue < bPair.second) {
+                        cPair = bPair
+                        bPair = Pair(newInput, newValue)
+                    } else {
+                        aPair = Pair(newInput, newValue)
+                    }
+
+
+                } else if (newInput > b.first) {
+                    if (newValue < b.second) {
+                        aPair = bPair
+                        bPair = Pair(newInput, newValue)
+                    } else {
+                        cPair = Pair(newInput, newValue)
+                    }
+                } else {
+                    val midAB = 0.5 * (a.first + b.first)
+                    val midValueAB = valueFunction(midAB)
+                    val midBC = 0.5 * (b.first + c.first)
+                    val midValueBC = valueFunction(midBC)
+
+                    if (midValueAB < b.second) {
+                        if (midValueBC < b.second) {
+                            if (midValueAB < midValueBC) {
+                                cPair = bPair
+                                bPair = Pair(midAB, midValueAB)
+                            } else {
+                                aPair = bPair
+                                bPair = Pair(midBC, midValueBC)
+                            }
+                        } else {
+                            cPair = bPair
+                            bPair = Pair(midAB, midValueAB)
+                        }
+
+                    } else if (midValueAB > b.second) {
+                        if (midValueBC < b.second) {
+                            aPair = bPair
+                            bPair = Pair(midBC, midValueBC)
+                        } else {
+                            aPair = Pair(midAB, midValueAB)
+                            cPair = Pair(midBC, midValueBC)
+                        }
+
+                    } else {
+                        if (midValueBC > b.second) {
+                            aPair = bPair
+                            bPair = Pair(midBC, midValueBC)
+                        } else {
+                            aPair = Pair(midAB, midValueAB)
+                            cPair = Pair(midBC, midValueBC)
+                        }
+                    }
+                }
+            }
+
+            return Pair(currentMinInput, currentMinValue)
+        }
+    }
 
 }
--- src/test/kotlin/com/kjipo/TestBisectMethod.kt
+++ src/test/kotlin/com/kjipo/TestBisectMethod.kt
@@ -7,7 +7,6 @@ import kotlin.test.assertTrue
 
 class TestBisectMethod {
 
-
     @Test
     fun testBisectMethodConverges() {
         val functionToMinimize = { value: Double ->
@@ -21,10 +20,49 @@ class TestBisectMethod {
             2.0
         )
 
-//        println("Input: $x. Value: ${functionToMinimize(x)}")
-
+        // This is a local maximum
         assertTrue { abs(x - 0.169938) < 0.0001 }
         assertTrue { abs(functionToMinimize(x) - 1.08414) < 0.0001 }
+    }
+
+    @Test
+    fun testGetBracketdMinimumMethodConverges() {
+        val functionToMinimize = { value: Double ->
+            value.pow(4) - 3 * value.pow(2) + value + 1
+        }
+
+        val aPair = Pair(-2.0, functionToMinimize(-2.0))
+        val bPair = Pair(0.0, functionToMinimize(0.0))
+        val cPair = Pair(2.0, functionToMinimize(2.0))
+        val currentMin = listOf(aPair, bPair, cPair).maxBy { it.second }
+
+        val (x, fx) = CylinderIntersectionUtilities.getBracketedMinimum(
+            functionToMinimize,
+            aPair,
+            bPair,
+            cPair,
+            currentMin
+        )
+
+        assertTrue { abs(x + 1.3008) < 0.0001 }
+        assertTrue { abs(fx + 2.5139) < 0.0001 }
+    }
+
+    @Test
+    fun testBisect2MethodConverges() {
+        val functionToMinimize = { value: Double ->
+            value.pow(4) - 3 * value.pow(2) + value + 1
+        }
+
+        val (x, fx) = CylinderIntersectionUtilities.bisect2(
+            functionToMinimize,
+            -2.0,
+            0.0,
+            2.0
+        )
+
+        assertTrue { abs(x + 1.3008) < 0.0001 }
+        assertTrue { abs(fx + 2.5139) < 0.0001 }
     }
 
 }
